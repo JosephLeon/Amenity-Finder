dac2fcafbabbdd8128d5bc703d68bbc6381a3f3d
Joseph Leon <joseph.leon.9@gmail.com>
2015-06-07 10:53:49 -0400

    Add GMSMapView to MapResultsViewController, clean up some storyboard stuff.

--- AmenityFinder/MapResultsViewController.swift
+++ AmenityFinder/MapResultsViewController.swift
@@ -14,17 +14,17 @@ class MapResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-            longitude: 151.20, zoom: 6)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view = mapView
-        
-        var marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+//        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+//            longitude: 151.20, zoom: 6)
+//        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+//        mapView.myLocationEnabled = true
+//        self.view = mapView
+//        
+//        var marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
