f7e46367ef6fff107b9511cb57ce8d399136be98
Joseph Leon <joseph.leon.9@gmail.com>
2015-06-12 17:33:31 -0400

    Add variables and sample category dictionary.

--- AmenityFinder/MapResultsViewController.swift
+++ AmenityFinder/MapResultsViewController.swift
@@ -12,6 +12,9 @@ import GoogleMaps
 class MapResultsViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var mapCenterPinImage: UIImageView!
+    @IBOutlet weak var pinImageVerticalConstraint: NSLayoutConstraint!
+    var searchedTypes = ["bakery", "bar", "cafe", "grocery_or_supermarket", "restaurant"]
 
     @IBAction func mapTypeSegmentPressed(sender: AnyObject) {
         let segmentedControl = sender as! UISegmentedControl
@@ -30,6 +33,26 @@ class MapResultsViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     let locationManager = CLLocationManager()
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    
+        if status == .AuthorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+        
+    }
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        if let location = locations.first as? CLLocation {
+            
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            locationManager.startUpdatingLocation()
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
